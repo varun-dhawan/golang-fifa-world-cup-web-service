d4f726f701f84763a4e29e56ee8540479884bfce
Varun Dhawan <varun.dhawan@target.com>
2020-04-03 08:41:36 -0500

    updated handlers.go

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -6,12 +6,13 @@ import (
 
 // RootHandler returns an empty body status code
 func RootHandler(res http.ResponseWriter, req *http.Request) {
+	res.WriteHeader(http.StatusNoContent)
 
 }
 
 // ListWinners returns winners from the list
 func ListWinners(res http.ResponseWriter, req *http.Request) {
-
+	res.Header().Set("Content-Type", "application/json")
 }
 
 // AddNewWinner adds new winner to the list
