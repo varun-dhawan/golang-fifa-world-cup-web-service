3f40675e3a80dec3b19c75343d5d38effc172809
Varun Dhawan <varun.dhawan@target.com>
2020-04-03 08:45:02 -0500

    task 3

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,18 +1,24 @@
 package handlers
 
 import (
+	"golang-fifa-world-cup-web-service/data"
 	"net/http"
 )
 
 // RootHandler returns an empty body status code
 func RootHandler(res http.ResponseWriter, req *http.Request) {
 	res.WriteHeader(http.StatusNoContent)
-
 }
 
 // ListWinners returns winners from the list
 func ListWinners(res http.ResponseWriter, req *http.Request) {
 	res.Header().Set("Content-Type", "application/json")
+
+	winner, err := data.ListAllJSON()
+	if err != nil {
+		res.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 }
 
 // AddNewWinner adds new winner to the list
