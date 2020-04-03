46f5282a5dfe18eaa969b4b273a7407a97c14c6f
Varun Dhawan <varun.dhawan@target.com>
2020-04-03 09:04:44 -0500

    activity 3 task 2

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -39,6 +39,8 @@ func AddNewWinner(res http.ResponseWriter, req *http.Request) {
 	isTokenValid := data.IsAccessTokenValid(accessToken)
 	if !isTokenValid {
 		res.WriteHeader(http.StatusUnauthorized)
+	} else {
+		res.WriteHeader(http.StatusCreated)
 	}
 }
 
